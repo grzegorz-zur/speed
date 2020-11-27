898f1367cc2b0cb471afcdc12a27f82e30b0f13a
Grzegorz Żur <grzegorz.zur@gmail.com>
2020-11-27 23:53:14 +0100

    Siplified service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,16 @@
-var name = "speed";
+const name = "speed";
   
-self.addEventListener("install", function (event) { 
-	event.waitUntil( 
-		caches.open(name).then(function (cache) { 
-			return cache.addAll(["/"]); 
-		}) 
-	); 
-}); 
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(name).then((cache) => {
+			return cache.addAll(["/"]);
+		})
+	);
+});
 
-self.addEventListener("fetch", function (event) { 
+self.addEventListener("fetch", (event) => {
 	event.respondWith(
-		caches.match(event.request).then(function (response) {
+		caches.match(event.request).then((response) => {
 			return response || fetch(event.request);
 		})
 	);
