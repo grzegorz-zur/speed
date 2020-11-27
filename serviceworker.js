18bc01fbbb76ceffd49000c44a65274936377580
Grzegorz Żur <grzegorz.zur@gmail.com>
2020-11-27 22:33:53 +0100

    Progressive Web Application

---
+++ serviceworker.js
@@ -0,0 +1,17 @@
+var name = "speed";
+  
+self.addEventListener("install", function (event) { 
+	event.waitUntil( 
+		caches.open(name).then(function (cache) { 
+			return cache.addAll(["/"]); 
+		}) 
+	); 
+}); 
+
+self.addEventListener("fetch", function (event) { 
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);
+});
